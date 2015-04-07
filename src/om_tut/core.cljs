fa3286bb91c1542e6e78822d92f668de4ebc21b4
prabhasp <prabhas.pokharel@gmail.com>
2015-04-07 18:18:34 +0545

    Adding the filter component; minor changes to clear completed styling

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -17,7 +17,8 @@
       {:text "Understand sablono" :done true}
       {:text "Become an Om master" :done false}
       {:text "Meditate" :done false}
-      {:text "Become a Front-end Ninja" :done false}]})
+      {:text "Become a Front-end Ninja" :done false}]
+     :filter :all})
    :todos-app-state))
 
 (defn todo-item [todo owner]
@@ -48,19 +49,34 @@
                                      (partial add-todo (.-value input)))
                        (set! (.-value input) ""))))}])))
 
-(defn bottom-bar [todos owner]
+(defn filter-display
+  [data owner]
   (om/component
    (html
-    (let [not-done-count (count (remove :done todos))
+    (let [filters {:all "All" :active "Active" :completed "Completed"}]
+      [:div.filters
+       (for [[key text] filters]
+         [:a {:href "#"
+              :class (when (= key (:filter data)) "active")
+              :on-click #(om/update! data :filter key)}
+          text])]))))
+
+(defn bottom-bar [data owner]
+  (om/component
+   (html
+    (let [todos (:todos data)
+          not-done-count (count (remove :done todos))
           done-count (count (filter :done todos))
           remove-done (fn [todos]
                         (vec (remove :done todos)))]
       [:div.bottom-bar
        (str not-done-count " items left")
-       (if (pos? done-count)
-         [:button
-          {:on-click #(om/transact! todos remove-done)}
-          "Clear completed"])]))))
+       (om/build filter-display data)
+       [:a
+          {:class (if (zero? done-count) "invisible")
+           :href "#"
+           :on-click #(om/transact! todos remove-done)}
+        "Clear completed"]]))))
 
 (defn mark-all-as-done [todos owner]
   (om/component
@@ -75,6 +91,16 @@
                     (partial mark-as (not all-done?)))}
        "✔"]))))
 
+(defn filter-todos
+  "Given filter-key (one of :all, :active, :completed) and todos,
+   return just the set of todos that should be visible."
+  [filter-key todos]
+  (println filter-key)
+  (case filter-key
+    :all todos
+    :active (vec (remove :done todos))
+    :completed (vec (filter :done todos))))
+
 (defn todo-list [data owner]
   (om/component
    (html
@@ -83,8 +109,10 @@
      [:div.top-bar
       (om/build mark-all-as-done (:todos data))
       (om/build todo-adder (:todos data))]
-     [:ul (om/build-all todo-item (:todos data))]
-     (om/build bottom-bar (:todos data))])))
+     [:ul
+      (om/build-all todo-item
+                    (filter-todos (:filter data) (:todos data)))]
+     (om/build bottom-bar data)])))
 
 (om/root todo-list
          app-state
